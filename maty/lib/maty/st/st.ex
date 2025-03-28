0d4b2b67d56b57b2676c8689594f0bd8f5085bd6
Petros <93052465+pkitazos@users.noreply.github.com>
2025-03-28 15:20:00 +0000

    wip: added structs for recursive session types

--- maty/lib/maty/st/st.ex
+++ maty/lib/maty/st/st.ex
@@ -2,6 +2,7 @@ defmodule Maty.ST do
   @type t :: SIn.t() | SOut.t() | SEnd.t() | SName.t() | SBranch.t()
 
   defmodule SBranch do
+    @moduledoc false
     @enforce_keys [:label, :payload, :continue_as]
     defstruct [:label, :payload, :continue_as]
 
@@ -13,6 +14,7 @@ defmodule Maty.ST do
   end
 
   defmodule SIn do
+    @moduledoc false
     @enforce_keys [:from, :branches]
     defstruct [:from, :branches]
 
@@ -23,6 +25,7 @@ defmodule Maty.ST do
   end
 
   defmodule SOut do
+    @moduledoc false
     @enforce_keys [:to, :branches]
     defstruct [:to, :branches]
 
@@ -33,17 +36,39 @@ defmodule Maty.ST do
   end
 
   defmodule SEnd do
+    @moduledoc false
     defstruct []
 
     @type t :: %__MODULE__{}
   end
 
   defmodule SName do
+    @moduledoc false
     @enforce_keys [:handler]
     defstruct [:handler]
 
     @type t :: %__MODULE__{
             handler: atom()
           }
+  end
+
+  defmodule SRec do
+    @moduledoc false
+    @enforce_keys [:label, :body]
+    defstruct [:label, :body, outer_recurse: false]
+
+    @type session_type() :: ST.session_type()
+    @type label() :: ST.label()
+    @type t :: %__MODULE__{label: label(), body: session_type(), outer_recurse: boolean()}
+  end
+
+  defmodule SRecLabel do
+    @moduledoc false
+    @enforce_keys [:label]
+    defstruct [:label]
+
+    @type session_type() :: ST.session_type()
+    @type label() :: ST.label()
+    @type t :: %__MODULE__{label: label()}
   end
 end
